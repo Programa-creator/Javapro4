b33b1680e303c8112c7bf8ec918b5718405ed89f
Programa-creator <116491894+Programa-creator@users.noreply.github.com>
2022-11-03 10:39:37 +0200

    Cats

--- src/main/java/org/example/Cat.java
+++ src/main/java/org/example/Cat.java
@@ -5,18 +5,13 @@ public class Cat extends Animals{
     protected String fill = "Фiлл";
 
     protected void catrun(String name, int distance) {
-        if (distance <= 500) {
+        if (distance <= 200) {
             System.out.print(name + " пробiг:" + distance + "м");
         } else {
             System.out.print(name + " не змiг пробiгти:" + distance + "м");
         }
     }
-
     protected void catswim(int distance, String name) {
-        if (distance <= 10) {
-            System.out.print(name + " проплив: " + distance + "м");
-        } else {
             System.out.print(name + " не змiг проплити: " + distance + "м");
-        }
     }
 }
